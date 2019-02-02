257fd610ecbe14b887f172ccd498aa18b2f7c6f7
Knut Zuidema <knut.zuidema@gmail.com>
2019-02-02 18:41:14 +0100

    add additional tracking parameters

--- pkg/model/elevator.go
+++ pkg/model/elevator.go
@@ -3,18 +3,20 @@ package model
 import "log"
 
 type Elevator struct {
-	Id              int
-	TotalPickedUp   int
-	Capacity        int
-	Persons         map[int]*Person
-	CurrentFloor    int
-	Destinations    map[int]bool
-	StepsTaken      int
-	OpenDoorsSignal chan interface{}
-	AscendSignal    chan interface{}
-	DescendSignal   chan interface{}
-	IdleSignal      chan interface{}
-	ContinueSignal  chan interface{}
+	Id                  int
+	TotalPickedUp       int
+	FloorsTraveledCount int
+	DoorsOpenedCount    int
+	IdleCount           int
+	Capacity            int
+	Persons             map[int]*Person
+	CurrentFloor        int
+	StepsTaken          int
+	OpenDoorsSignal     chan interface{}
+	AscendSignal        chan interface{}
+	DescendSignal       chan interface{}
+	IdleSignal          chan interface{}
+	ContinueSignal      chan interface{}
 }
 
 func NewElevator(id int, capacity int) *Elevator {
@@ -22,7 +24,6 @@ func NewElevator(id int, capacity int) *Elevator {
 		Id:              id,
 		Capacity:        capacity,
 		Persons:         map[int]*Person{},
-		Destinations:    map[int]bool{},
 		OpenDoorsSignal: make(chan interface{}),
 		AscendSignal:    make(chan interface{}),
 		DescendSignal:   make(chan interface{}),
@@ -35,20 +36,24 @@ func (e *Elevator) Run(controller *Controller) {
 	for {
 		select {
 		case <-e.OpenDoorsSignal:
+			e.DoorsOpenedCount++
 			droppedOff := e.dropOff(e.CurrentFloor)
 			log.Printf("elevator %04v: dropped off %v persons on floor %v", e.Id, droppedOff, e.CurrentFloor)
 			pickedUp := e.pickUp(controller.Floors[e.CurrentFloor])
 			log.Printf("elevator %04v: picked up %v persons on floor %v", e.Id, pickedUp, e.CurrentFloor)
 			e.StepsTaken++
 		case <-e.AscendSignal:
+			e.FloorsTraveledCount++
 			// log.Printf("elevator %04v: ascending from floor %v", e.Id, e.CurrentFloor)
 			e.CurrentFloor++
 			e.StepsTaken++
 		case <-e.DescendSignal:
+			e.FloorsTraveledCount++
 			// log.Printf("elevator %04v: descending from floor %v", e.Id, e.CurrentFloor)
 			e.CurrentFloor--
 			e.StepsTaken++
 		case <-e.IdleSignal:
+			e.IdleCount++
 		}
 		e.ContinueSignal <- nil
 	}
